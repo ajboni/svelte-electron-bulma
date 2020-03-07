f5263c1f42e963e3cf8772876930d6d9f5f25557
aboni <mail@aboni.dev>
2020-03-07 01:44:19 -0300

    Demo svelte content

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,52 @@
 <script>
   export let name;
+  import { slide, fade, fly } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
+  let read = false;
+  function readNotification() {
+    read = !read;
+  }
 </script>
 
 <main>
 
-  <div class="tile is-ancestor">
-    <div class="tile is-vertical is-8">
-      <div class="tile">
-        <div class="tile is-parent is-vertical">
-          <article class="tile is-child notification is-primary">
-            <p class="title">Vertical...</p>
-            <p class="subtitle">Top tile</p>
-          </article>
-          <article class="tile is-child notification is-warning">
-            <p class="title">...tiles</p>
-            <p class="subtitle">Bottom tile</p>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-info">
-            <p class="title">Middle tile</p>
-            <p class="subtitle">With an image</p>
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/640x480.png" />
-            </figure>
-          </article>
-        </div>
+  {#if read}
+    <!-- content here -->
+    <section
+      in:fly={{ delay: 1000, x: 200, duration: 2000 }}
+      out:fade={{ delay: 0 }}
+      class="section">
+      <div class="container">
+        <h1 class="title">Section</h1>
+        <h2 class="subtitle">
+          A simple container to divide your page into
+          <strong>sections</strong>
+          , like the one you're currently reading
+        </h2>
+        <button
+          class="button is-outlined"
+          on:click={readNotification}
+          in:fade={{ delay: 4000 }}>
+          Okay.
+        </button>
+
       </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-danger">
-          <p class="title">Wide tile</p>
-          <p class="subtitle">Aligned with the right tile</p>
-          <div class="content">
-            <!-- Content -->
-          </div>
-        </article>
-      </div>
-    </div>
-    <div class="tile is-parent">
-      <article class="tile is-child notification is-success">
-        <div class="content">
-          <p class="title">Tall tile</p>
-          <p class="subtitle">With even more content</p>
-          <div class="content">
-            <!-- Content -->
+    </section>
+  {:else}
+    <!-- content here -->
+    <section
+      in:slide={{ delay: 1000, duration: 3000, easing: quintOut }}
+      out:fade
+      class="section">
+      <div class="container">
+        <div class="notification">
+          <div class="notification">
+            <button on:click={readNotification} class="delete" />
+            Read me and discard me.
           </div>
         </div>
-      </article>
-    </div>
-  </div>
+      </div>
+    </section>
+  {/if}
 
 </main>
